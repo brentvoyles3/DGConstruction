b1201287cefd5a576c8d6c25935d586e8efa2966
Brent+ Voyles <brent.voyles3@uga.edu>
2023-12-12 18:21:53 -0500

    added landing page

--- fulldeckconstruction/src/App.js
+++ fulldeckconstruction/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {/* This is the alias of BrowserRouter i.e. Router */}
+        <Router>
+            <Routes>
+                {/* This route is for home component 
+      with exact path "/", in component props 
+      we passes the imported component*/}
+     <Route
+                      exact path="/"
+                      element={<Home />}
+                  />
+                  {/* If any route mismatches the upper 
+        route endpoints then, redirect triggers 
+        and redirects app to home component with to="/" */}
+                  {/* <Redirect to="/" /> */}
+                  <Route
+                      path="*"
+                      element={<Navigate to="/" />}
+                  />
+              </Routes>
+          </Router>
+      </>
   );
 }
 
 export default App;
+
---
+++ fulldeckconstruction/src/components/Home.js
@@ -0,0 +1,25 @@
+// Filename - Home.jsx
+import React from "react";
+// Importing Link from react-router-dom to 
+// navigate to different end points.
+import { Link } from "react-router-dom";
+import "../App.css"
+import Deck from "../deck.jpg"
+ 
+const Home = () => {
+    return (
+        <div className="landingBody">
+            <div className="picContainer">
+                <img src={Deck} className = "Deck"></img>
+            </div>
+             <div className="landingContainer">
+        <article>
+          <h1>Learn about Full Deck Construction </h1>
+          Full Deck Construction stands as a beacon of craftsmanship in the world of construction, where every nail and board is a testament to their commitment to excellence. With a team of skilled artisans at the helm, Full Deck takes pride in their talented carpenters who bring dreams to life, one plank at a time. These craftsmen don't just build structures; they sculpt spaces, transforming visions into solid, tangible realities. Full Deck Construction doesn't just promise sturdy buildings; they promise a symphony of skill and precision, ensuring that each project is a masterpiece crafted by the hands of seasoned carpentry virtuosos.
+        </article>
+        </div>        
+        </div>
+    );
+};
+ 
+export default Home;
